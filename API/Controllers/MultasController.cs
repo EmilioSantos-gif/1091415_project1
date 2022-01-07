286309f6d6e78384f91196dec723ff56f0cced5f
Emilio Santos <emilioariel1018@hotmail.com>
2022-01-07 18:02:03 -0400

    Pushing Final Project

---
+++ API/Controllers/MultasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BO;
+
+namespace API.Controllers
+{
+    public class MultasController : ApiController
+    {
+        private MultaEntities1 db = new MultaEntities1();
+
+        // GET: api/Multas
+        public IQueryable<Multa> GetMulta()
+        {
+            return db.Multa;
+        }
+
+        // GET: api/Multas/5
+        [ResponseType(typeof(Multa))]
+        public IHttpActionResult GetMulta(string id)
+        {
+            Multa multa = db.Multa.Find(id);
+            if (multa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(multa);
+        }
+
+        // PUT: api/Multas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMulta(string id, Multa multa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != multa.Acta)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(multa).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MultaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Multas
+        [ResponseType(typeof(Multa))]
+        public IHttpActionResult PostMulta(Multa multa)
+        {
+            multa.Id = Guid.NewGuid();
+            multa.Fecha = DateTime.Now;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Multa.Add(multa);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (MultaExists(multa.Acta))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = multa.Acta }, multa);
+        }
+
+        // DELETE: api/Multas/5
+        [ResponseType(typeof(Multa))]
+        public IHttpActionResult DeleteMulta(string id)
+        {
+            Multa multa = db.Multa.Find(id);
+            if (multa == null)
+            {
+                return NotFound();
+            }
+
+            db.Multa.Remove(multa);
+            db.SaveChanges();
+
+            return Ok(multa);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MultaExists(string id)
+        {
+            return db.Multa.Count(e => e.Acta == id) > 0;
+        }
+    }
+}
---
+++ UI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.multaDataSet = new UI.MultaDataSet();
+            this.multaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.multaTableAdapter = new UI.MultaDataSetTableAdapters.MultaTableAdapter();
+            this.actaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cedulaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.infraccionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estadoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.actaDataGridViewTextBoxColumn,
+            this.nombreDataGridViewTextBoxColumn,
+            this.apellidoDataGridViewTextBoxColumn,
+            this.cedulaDataGridViewTextBoxColumn,
+            this.infraccionDataGridViewTextBoxColumn,
+            this.estadoDataGridViewTextBoxColumn,
+            this.precioDataGridViewTextBoxColumn,
+            this.fechaDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.multaBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1053, 152);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // multaDataSet
+            // 
+            this.multaDataSet.DataSetName = "MultaDataSet";
+            this.multaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // multaBindingSource
+            // 
+            this.multaBindingSource.DataMember = "Multa";
+            this.multaBindingSource.DataSource = this.multaDataSet;
+            // 
+            // multaTableAdapter
+            // 
+            this.multaTableAdapter.ClearBeforeFill = true;
+            // 
+            // actaDataGridViewTextBoxColumn
+            // 
+            this.actaDataGridViewTextBoxColumn.DataPropertyName = "Acta";
+            this.actaDataGridViewTextBoxColumn.HeaderText = "Acta";
+            this.actaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.actaDataGridViewTextBoxColumn.Name = "actaDataGridViewTextBoxColumn";
+            this.actaDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nombreDataGridViewTextBoxColumn
+            // 
+            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
+            this.nombreDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // apellidoDataGridViewTextBoxColumn
+            // 
+            this.apellidoDataGridViewTextBoxColumn.DataPropertyName = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.HeaderText = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.apellidoDataGridViewTextBoxColumn.Name = "apellidoDataGridViewTextBoxColumn";
+            this.apellidoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // cedulaDataGridViewTextBoxColumn
+            // 
+            this.cedulaDataGridViewTextBoxColumn.DataPropertyName = "Cedula";
+            this.cedulaDataGridViewTextBoxColumn.HeaderText = "Cedula";
+            this.cedulaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.cedulaDataGridViewTextBoxColumn.Name = "cedulaDataGridViewTextBoxColumn";
+            this.cedulaDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // infraccionDataGridViewTextBoxColumn
+            // 
+            this.infraccionDataGridViewTextBoxColumn.DataPropertyName = "Infraccion";
+            this.infraccionDataGridViewTextBoxColumn.HeaderText = "Infraccion";
+            this.infraccionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.infraccionDataGridViewTextBoxColumn.Name = "infraccionDataGridViewTextBoxColumn";
+            this.infraccionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // estadoDataGridViewTextBoxColumn
+            // 
+            this.estadoDataGridViewTextBoxColumn.DataPropertyName = "Estado";
+            this.estadoDataGridViewTextBoxColumn.HeaderText = "Estado";
+            this.estadoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.estadoDataGridViewTextBoxColumn.Name = "estadoDataGridViewTextBoxColumn";
+            this.estadoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // precioDataGridViewTextBoxColumn
+            // 
+            this.precioDataGridViewTextBoxColumn.DataPropertyName = "Precio";
+            this.precioDataGridViewTextBoxColumn.HeaderText = "Precio";
+            this.precioDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.precioDataGridViewTextBoxColumn.Name = "precioDataGridViewTextBoxColumn";
+            this.precioDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // fechaDataGridViewTextBoxColumn
+            // 
+            this.fechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
+            this.fechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
+            this.fechaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fechaDataGridViewTextBoxColumn.Name = "fechaDataGridViewTextBoxColumn";
+            this.fechaDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MultaDataSet multaDataSet;
+        private System.Windows.Forms.BindingSource multaBindingSource;
+        private MultaDataSetTableAdapters.MultaTableAdapter multaTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn actaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellidoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cedulaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn infraccionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estadoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaDataGridViewTextBoxColumn;
+    }
+}
+
